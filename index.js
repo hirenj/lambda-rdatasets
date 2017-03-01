7caa445a44d375f3021fdf7da79b55dfc2fe60e8
Hiren Joshi <hirenj@gmail.com>
2017-03-01 22:39:14 +0100

    Add in basic methods to retrieve data

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ if (config.region) {
 }
 const AWS = require('lambda-helpers').AWS;
 const s3 = new AWS.S3();
+const RData = require('node-rdata');
+const JSONStream = require('JSONStream');
+const fs = require('fs');
 
 const extract_changed_keys = function(event) {
   if ( ! event.Records ) {
@@ -29,6 +32,51 @@ const extract_changed_keys = function(event) {
   });
   results = [].concat.apply([],results);
   return results.filter( obj => obj.bucket == bucket_name ).map( obj => obj.key );
+};
+
+const retrieve_file_s3 = function retrieve_file_s3(filekey,byte_offset) {
+  let params = {
+    'Key' : filekey,
+    'Bucket' : bucket_name
+  };
+  if (byte_offset) {
+    params.Range = 'bytes='+byte_offset+'-';
+  }
+  let request = s3.getObject(params);
+  let stream = request.createReadStream();
+  return stream;
+};
+
+const retrieve_file_local = function retrieve_file_local(filekey) {
+  return fs.createReadStream(filekey);
+};
+
+const retrieve_file = function retrieve_file(filekey,md5_result,byte_offset) {
+  return retrieve_file_s3(filekey,md5_result,byte_offset);
+};
+
+const read_data_stream = function(path) {
+  let input_stream = retrieve_file(path);
+  let entry_data = input_stream.pipe(JSONStream.parse(['data', {'emitKey': true}]));
+  return entry_data;
+};
+
+const write_frame_stream = function(json_stream) {
+  let typeinfo =  {   'type': 'dataframe',
+            'keys' : ['x','y','z'],
+            'types' : ['int','string','logical']
+          };
+
+  let output = require('fs').createWriteStream('output.Rdata');
+
+  let writer = new RData(output);
+
+  // Write the header for the R data file format
+  writer.writeHeader();
+
+  // We need to write out the data frame into an environment
+  writer.environment( {'data' : json_stream },{'data' : typeinfo })
+      .then( () => writer.finish() );
 };
 
 const serialiseDataset = function(event,context) {
