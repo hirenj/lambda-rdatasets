49daeab9af8e75ee80bdf747053f1e8f78ef60a7
Hiren Joshi <hirenj@gmail.com>
2017-03-30 16:15:20 +0200

    Add writing of files to s3

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ const ConvertJSON = require('./js/transform').ConvertJSON;
 const msdata = require('./js/msdata');
 const jsonstreamer = require('node-jsonpath-s3');
 const metaConverter = require('node-uberon-mappings');
+const zlib = require('zlib');
+const temp = require('temp');
+const AWS = require('lambda-helpers').AWS;
+const fs = require('fs');
+
 
 const choose_transform = function(metadata) {
   if (metadata.mimetype == 'application/json+msdata') {
@@ -126,16 +131,23 @@ const write_frame_stream = function(json_stream,metadata) {
     });
   });
 
-  let output = require('fs').createWriteStream('output.Rdata');
+  let gz = zlib.createGzip();
 
-  let writer = new RData(output);
+  let writer = new RData(gz);
+
+
+  let outstream = temp.createWriteStream();
+  let output_path = outstream.path;
+
+  gz.pipe(outstream);
 
   // Write the header for the R data file format
   writer.writeHeader();
 
   // We need to write out the data frame into an environment
   return writer.environment( {'data' : json_stream },{'data' : typeinfo })
-      .then( () => writer.finish() );
+      .then( () => writer.finish() )
+      .then( () => output_path );
 };
 
 
@@ -146,18 +158,67 @@ const do_transform = function(filename,metadata) {
     let transformer = choose_transform(metadata);
     return write_frame_stream( stream.pipe(new ConvertJSON(transformer)), metadata );
   })
-  .then( () => {
-    console.log('All done');
-  })
   .catch( console.log.bind(console) );
+};
+
+
+const uploadToS3 = function(target) {
+  var pass = new PassThrough();
+
+  var params = {Bucket: bucket_name, Key: target, Body: pass};
+  let s3 = new AWS.S3();
+  console.log(params);
+  pass.finished = new Promise( (resolve,reject) => {
+    s3.upload(params, function(err, data) {
+      console.log(arguments);
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+  return pass;
+};
+
+const transformDataS3 = function(input_key,target_key) {
+  return do_transform(input_key).then( filename => {
+    let output_pipe = uploadToS3(target_key);
+    fs.createReadStream(filename).pipe(output_pipe);
+    return output_pipe.finished;
+  });
+};
+
+const transformDataLocal = function(input_key) {
+  return do_transform(input_key).then( filename => {
+    console.log('Filename is',filename);
+    let output_pipe = fs.createWriteStream('output.Rdata');
+    let writer = fs.createReadStream(filename);
+    writer.pipe(output_pipe);
+    return new Promise( (resolve,reject) => {
+      writer.on('finish',resolve);
+      writer.on('error',reject);
+    });
+  });
 };
 
 const serialiseDataset = function(event,context) {
   let changed_keys = extract_changed_keys(event);
-  console.log(changed_keys);
-  Promise.resolve().then( () => {
+  if (changed_keys.length < 1) {
     context.succeed('OK');
-  }).catch( err => {
+    return;
+  }
+  console.log(changed_keys);
+  let key = changed_keys[0];
+  let output_key = key.split('/')[1];
+  if ( ! output_key ) {
+    console.log('Missing output key for',key);
+    context.fail('NOT-OK');
+    return;
+  }
+  transformDataS3(`s3://${bucket_name}/${key}`,`rdata/${output_key}`)
+  .then( () => context.succeed('OK') )
+  .catch( err => {
     console.error(err);
     console.error(err.stack);
     context.fail('NOT-OK');
@@ -165,4 +226,5 @@ const serialiseDataset = function(event,context) {
 };
 
 exports.serialiseDataset = serialiseDataset;
-exports.do_transform = do_transform;
+exports.do_transform = transformDataLocal;
+exports.do_transform_s3 = transformDataS3;
