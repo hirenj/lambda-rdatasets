30b58867b70f03665bb3b5a284d6c86ea71571e9
Hiren Joshi <hirenj@gmail.com>
2017-09-22 16:04:55 +0200

    Update metadata when running a single run

--- run_single.js
+++ run_single.js
@@ -3,6 +3,9 @@
 let runner = require('.');
 let bucket_name = process.env['BUILD_BUCKET'];
 let key = process.argv[2];
+let output_key = key.split('/')[1];
 let serialiser = process.argv[3];
 console.log(`Reading file from s3://${bucket_name}/${key} and writing to rdata/`)
-runner.do_transform_s3(serialiser,`s3://${bucket_name}/${key}`,'rdata/').then( () => console.log("OK"));
+runner.do_transform_s3(serialiser,`s3://${bucket_name}/${key}`,'rdata/')
+      .then( (filedata) => runner.update_metadata(output_key,`${filedata.title}_${filedata.version}`) )
+      .then( () => console.log("OK"));
