5e765661f3510db7a7022fd2a1c6a87af261a01e
Hiren Joshi <hirenj@gmail.com>
2019-03-29 17:02:40 +0100

    Make script crash out if there is an error

--- run_single.js
+++ run_single.js
@@ -9,4 +9,4 @@ console.log(`Reading file from s3://${bucket_name}/${key} and writing to rdata/`
 runner.do_transform_s3(serialiser,`s3://${bucket_name}/${key}`,'rdata/')
       .then( (filedata) => runner.update_metadata(output_key,`${filedata.title}_${filedata.version}`) )
       .then( () => console.log("OK"))
-      .catch( err => console.log(err) );
+      .catch( err => { console.log(err); process.exit(1); });
