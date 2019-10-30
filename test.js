53be1514f134b4fffbb46b453a51ae1dedd1390e
Hiren Joshi <hirenj@gmail.com>
2019-10-30 09:52:55 +0100

    Default to generating RData

--- test.js
+++ test.js
@@ -2,6 +2,6 @@
 
 let runner = require('.');
 console.log(process.argv[2]);
-runner.do_transform('TDE_partial', process.argv[2].indexOf('//') < 0 ? ('file://'+process.argv[2]) : process.argv[2],'rdata/').catch( err =>
+runner.do_transform('RData', process.argv[2].indexOf('//') < 0 ? ('file://'+process.argv[2]) : process.argv[2],'rdata/').catch( err =>
   console.log(err)
 );
